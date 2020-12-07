17d49b549942eb1552c66c63d237d63d528ce1e4
Giovanni Carmo <giovannicarmo96@gmail.com>
2020-12-07 08:29:12 -0300

    Initial commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> User { get; set; }
+    }
+}
